85dfc1bb739c3d417be425fc341ae397659c9e77
essenlive <perchais.quentin@gmail.com>
2017-07-21 19:18:18 +0000

    qdded launch script for cron

--- print.py
+++ print.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 
 import time, sys, os
 from epsonprinter import EpsonPrinter
@@ -9,30 +8,52 @@ import RPi.GPIO as GPIO
 import time
 
 printer = init_printer(0x04b8, 0x0e15, 1)
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(21, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-input = 0
+GPIO.setup(20, GPIO.OUT)
+
+input = 1
+os.system('fswebcam --set brightness=100% --set "Exposure"="Manual Mode" --set "Exposure (Absolute)"="100%" ')
+
 while True:
+	GPIO.output(20, 1)  
 	input = GPIO.input(21)
 	if (input == 0):
-		printer.linefeed(2)
-		os.system('fswebcam --set brightness=100% --set "Exposure"="Manual Mode" --set "Exposure (Absolute)"="100%" ')
-                os.system('fswebcam -r 500x500 --no-banner cheese.jpg  ')
-
-		img = Image.open('cheese.jpg')
-		enhancer = ImageEnhance.Brightness(img)
-		enhancer.enhance(0.9).save("cheese2", "JPEG")
-
-
-
-		printer.print_image_from_file(img)	
+		GPIO.output(20, 0)
 		printer.linefeed(3)
+
+                now = str(int(time.time()*1000))
+
+
+                os.system('fswebcam -r 500x500 --no-banner --rotate 180 images/image1-'+now+'.jpg  ')
+          	time.sleep(0.5)                
+		os.system('fswebcam -r 500x500 --no-banner --rotate 180 images/image2-'+now+'.jpg   ')
+	        time.sleep(0.5)
+                os.system('fswebcam -r 500x500 --no-banner --rotate 180 images/image3-'+now+'.jpg   ')
+        	time.sleep(0.5)
+                os.system('fswebcam -r 500x500 --no-banner --rotate 180 images/image4-'+now+'.jpg   ')
+		
+		logo = Image.open("images/logo.PNG")
+		img1 = Image.open('images/image1-' + now + '.jpg')
+                img2 = Image.open('images/image2-' + now + '.jpg')
+                img3 = Image.open('images/image3-' + now + '.jpg')
+                img4 = Image.open('images/image4-' + now + '.jpg')
+		
+                printer.print_image_from_file(logo)
+                printer.linefeed(2)
+		printer.print_image_from_file(img1)
+                printer.linefeed(2)
+                printer.print_image_from_file(img2)
+                printer.linefeed(2)
+                printer.print_image_from_file(img3)
+                printer.linefeed(2)
+                printer.print_image_from_file(img4)
+		printer.linefeed(5)
+		
 		printer.cut()
-		if (brightness(img) > 0):
-                        now = str(int(time.time()))
-			img.save("images/image-"+now, "JPEG")
-		print brightness(img)
-		print "wait"
+		printer.linefeed(3)
+
 		time.sleep(1)
 		input = 1
-		print "go"
+	        print "Ready for capture"
